313a3be0c8ae80c8a9abc59a594e03d068a53338
iva-romanenko <iva-romanenko@yandex-team.ru>
2025-07-07 23:43:05 +0300

    clean old version

--- kubernetes_marketplace.go
+++
@@ -1,7 +0,0 @@
-package ycsdk
-
-import "github.com/yandex-cloud/go-sdk/gen/kubernetes/marketplace"
-
-func (sdk *SDK) KubernetesMarketplace() *marketplace.KubernetesMarketplace {
-	return marketplace.NewKubernetesMarketplace(sdk.getConn(KubernetesServiceID))
-}
